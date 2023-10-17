68a64f6005b95015857da0cc275d4545f1701f0c
Eddym31 <120415029+Eddym31@users.noreply.github.com>
2023-10-17 20:07:54 +0300

    Update settings.py

--- lista_cumparaturi/settings.py
+++ lista_cumparaturi/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x8fpschrzhmlfqqf*@5#9iu8)@_mwld7j1h7wf_mg4kc#&bwef'
+SECRET_KEY = 'enter key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
